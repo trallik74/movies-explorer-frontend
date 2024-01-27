4ae25bdf84792cab41fc53a64c856ff1a757b83e
trallik <trallik74@gmail.com>
2024-01-27 14:43:00 +0400

    feat: add page not found navigate

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,12 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import "./PageNotFound.css";
 
 function PageNotFound() {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
   return (
     <div className="page__content">
       <main className="page-not-found">
         <p className="page-not-found__error">404</p>
         <p className="page-not-found__message">Страница не найдена</p>
-        <button className="page-not-found__button">Назад</button>
+        <button
+          className="page-not-found__button"
+          onClick={goBack}
+          type="button"
+        >
+          Назад
+        </button>
       </main>
     </div>
   );
