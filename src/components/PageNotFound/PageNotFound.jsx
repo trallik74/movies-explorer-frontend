5e585f628edfad15d280d7fe665e581b44f979bf
trallik <trallik74@gmail.com>
2024-01-30 23:52:45 +0400

    feat: add to PageNotFound semantic elements

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -11,8 +11,13 @@ function PageNotFound() {
   return (
     <main className="page-not-found">
       <p className="page-not-found__error">404</p>
-      <p className="page-not-found__message">Страница не найдена</p>
-      <button className="page-not-found__button" onClick={goBack} type="button">
+      <h2 className="page-not-found__message">Страница не найдена</h2>
+      <button
+        className="page-not-found__button"
+        onClick={goBack}
+        type="button"
+        aria-label="Кнопка возврата на предыдущую страницу"
+      >
         Назад
       </button>
     </main>
