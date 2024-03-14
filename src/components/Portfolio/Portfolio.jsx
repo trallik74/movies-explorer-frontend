fdb604cf94912073193a068b7438aeeeca85d067
trallik <trallik74@gmail.com>
2024-03-14 14:13:51 +0400

    feat: change bio info and some link

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,33 +8,22 @@ function Portfolio() {
         <li className="portfolio__link-list-item">
           <a
             className="portfolio__link portfolio__link_type_github"
-            href="https://github.com/trallik74/how-to-learn"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p className="portfolio__link-text">Статичный сайт</p>
-            <div className="portfolio__arrow-icon" />
-          </a>
-        </li>
-        <li className="portfolio__link-list-item">
-          <a
-            className="portfolio__link portfolio__link_type_github"
-            href="https://github.com/trallik74/russian-travel"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <p className="portfolio__link-text">Адаптивный сайт</p>
-            <div className="portfolio__arrow-icon" />
-          </a>
-        </li>
-        <li className="portfolio__link-list-item">
-          <a
-            className="portfolio__link portfolio__link_type_github"
             href="https://github.com/trallik74/react-mesto-api-full-gha"
             target="_blank"
             rel="noreferrer"
           >
             <p className="portfolio__link-text">Одностраничное приложение</p>
+            <div className="portfolio__arrow-icon" />
+          </a>
+        </li>
+        <li className="portfolio__link-list-item">
+          <a
+            className="portfolio__link portfolio__link_type_github"
+            href="https://github.com/trallik74/neira-kp-calculator"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <p className="portfolio__link-text">Калькулятор коммерческого предложения</p>
             <div className="portfolio__arrow-icon" />
           </a>
         </li>
